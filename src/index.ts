9ddd024f640ebe38a60af375d8427f3e0683e0b7
Erick El nino <erick.tshimbombo@yahoo.com>
2024-06-18 23:40:55 +0200

    structure our app

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello world");
+import express from "express";
+import dotenv from "dotenv";
+import http from "http";
+dotenv.config();
+
+const app = express();
+
+const server: http.Server<
+  typeof http.IncomingMessage,
+  typeof http.ServerResponse
+> = http.createServer(app);
+
+server.listen(process.env.PORT, () => {
+  console.log(`Server is running on PORT: ${process.env.PORT}`);
+});
