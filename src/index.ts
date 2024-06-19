bcd6686e938fc63b83c8207148e5e944abbd62f0
Erick El nino <erick.tshimbombo@yahoo.com>
2024-06-20 00:17:09 +0200

    added prisma

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import http from "http";
 import userRouter from "./routes/userRoutes";
 import tweetRoutes from "./routes/tweetRoutes";
+import cors from "cors";
 dotenv.config();
 
 const app = express();
@@ -13,11 +14,22 @@ const server: http.Server<
 > = http.createServer(app);
 
 app.get("/", (request, response) => {
-  return response.send("<h1>Home</h1>");
+  return response.send("<h1>Welcome to our App</h1>");
 });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 // endpoints to get all ursers
+// =============================
 app.use("/users", userRouter);
+
+// ==============================
+// Tweets endpoints
+
 app.use("/tweets", tweetRoutes);
+
+// ==========================
 
 server.listen(process.env.PORT, () => {
   console.log(`Server is running on PORT: ${process.env.PORT}`);
