bbba11c174e93817abcb4e6fa4048fa666641366
Erick El nino <erick.tshimbombo@yahoo.com>
2024-06-19 22:03:11 +0200

    added all the endpoints related to the user

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { response } from "express";
 import dotenv from "dotenv";
 import http from "http";
 dotenv.config();
@@ -11,8 +11,48 @@ const server: http.Server<
 > = http.createServer(app);
 
 app.get("/", (request, response) => {
-  return response.send("Hello world");
+  return response.send("<h1>Home</h1>");
 });
+
+app.post("/users", (request: express.Request, repsonse: express.Response) => {
+  return response.status(501).json({
+    error: " not impelemented",
+  });
+});
+
+app.get("/user", (request: express.Request, repsonse: express.Response) => {
+  return response.status(501).json({
+    data: "No users",
+  });
+});
+
+app.get(
+  "/users/:id",
+  (request: express.Request, repsonse: express.Response) => {
+    return response.status(501).json({
+      data: "no user found",
+    });
+  }
+);
+
+app.put(
+  "/users/:id",
+  (request: express.Request, repsonse: express.Response) => {
+    return response.status(501).json({
+      data: "no user found",
+    });
+  }
+);
+
+app.delete(
+  "/users/:id",
+  (request: express.Request, repsonse: express.Response) => {
+    return response.status(501).json({
+      data: "no user found",
+    });
+  }
+);
+
 server.listen(process.env.PORT, () => {
   console.log(`Server is running on PORT: ${process.env.PORT}`);
 });
