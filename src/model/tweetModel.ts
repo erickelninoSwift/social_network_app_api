71bb917f727c6215b0417c4e1780d5442161f0e8
Erick El nino <erick.tshimbombo@yahoo.com>
2024-06-22 17:37:08 +0200

    updated timestamp required field

--- src/model/tweetModel.ts
+++ src/model/tweetModel.ts
@@ -2,26 +2,36 @@ import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
-export const tweetSchema = new Schema({
-  user: {
-    type: Schema.Types.ObjectId,
-    userID: { type: String, required: true },
-    user: { type: String, required: true },
-    required: true,
+interface tweetuser {
+  userId: String;
+  username: String;
+}
+
+interface tweet {
+  user: tweetuser;
+}
+
+export const tweetSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      userID: { type: String, required: true },
+      user: { type: String, required: true },
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    likes: {
+      type: Number,
+      default: 0,
+    },
   },
-  text: {
-    type: String,
-    required: true,
-    maxlength: 280,
-  },
-  likes: {
-    type: Number,
-    default: 0,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 export default mongoose.model("Tweets", tweetSchema);
--- src/model/userModel.ts
+++ src/model/userModel.ts
@@ -11,50 +11,49 @@ interface IUser {
   isVerified: false;
   tweetsId: String[];
 }
-const user = new mongoose.Schema<IUser>({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    minlength: 3,
-    maxlength: 20,
+const user = new mongoose.Schema<IUser>(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      minlength: 3,
+      maxlength: 20,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      match: /^\S+@\S+\.\S+$/,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      maxlength: 50,
+    },
+    bio: {
+      type: String,
+      maxlength: 160,
+    },
+    avatar: {
+      type: String,
+      default: "default_avatar.jpg",
+    },
+    isVerified: { type: Boolean, default: false },
+    tweetsId: {
+      type: [String],
+      required: true,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    lowercase: true,
-    match: /^\S+@\S+\.\S+$/,
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 6,
-  },
-  fullname: {
-    type: String,
-    required: true,
-    maxlength: 50,
-  },
-  bio: {
-    type: String,
-    maxlength: 160,
-  },
-  avatar: {
-    type: String,
-    default: "default_avatar.jpg",
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  isVerified: { type: Boolean, default: false },
-  tweetsId: {
-    type: [String],
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 export default mongoose.model("Users", user);
