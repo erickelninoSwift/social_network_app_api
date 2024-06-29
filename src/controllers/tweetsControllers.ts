5004d6d28ed3d23974f1df3dda2dbc5b1b959561
Erick El nino <erick.tshimbombo@yahoo.com>
2024-06-29 19:38:09 +0200

    added more tweets endpoints

--- src/controllers/tweetsControllers.ts
+++ src/controllers/tweetsControllers.ts
@@ -52,3 +52,53 @@ export const createTweet = async (
     });
   }
 };
+
+export const getTweetbyID = async (
+  request: express.Request,
+  response: express.Response
+) => {
+  const { tweetid } = request.params;
+  console.log(tweetid);
+  try {
+    const tweet = await tweetModel.findOne({ _id: tweetid });
+    if (!tweet) {
+      return response.status(404).json({
+        message: `Tweet with ID: ${tweetid} was not found`,
+      });
+    }
+    return response.status(200).json({
+      status: "success",
+      data: tweet,
+    });
+  } catch (error) {
+    return response.json({
+      message: error.message,
+    });
+  }
+};
+
+export const deleteTweetByID = async (
+  request: express.Request,
+  response: express.Response
+) => {
+  const { id } = request.params;
+  try {
+    const gettweet = await tweetModel.findOne({ _id: id });
+    if (!gettweet) {
+      return response.status(404).json({
+        message: "tweet was not found",
+      });
+    }
+    const deleteTweet = await tweetModel.findOneAndDelete({ _id: id });
+
+    return response.status(201).json({
+      status: "deleted",
+      data: deleteTweet,
+    });
+  } catch (error) {
+    return response.status(501).json({
+      status: "node found",
+      error: error.message,
+    });
+  }
+};
--- src/routes/tweetRoutes.ts
+++ src/routes/tweetRoutes.ts
@@ -1,29 +1,16 @@
 import express, { Router } from "express";
-import { getAllTweets, createTweet } from "../controllers/tweetsControllers";
+import {
+  getAllTweets,
+  createTweet,
+  getTweetbyID,
+  deleteTweetByID,
+} from "../controllers/tweetsControllers";
 const router: Router = express.Router();
 
 router.get("/", getAllTweets);
 router.post("/", createTweet);
-
-router.get("/:id", (request: express.Request, response: express.Response) => {
-  return response.status(501).json({
-    data: "no tweet found",
-  });
-});
-
-router.put("/:id", (request: express.Request, response: express.Response) => {
-  return response.status(501).json({
-    data: "no tweet found",
-  });
-});
-
-router.delete(
-  "/:id",
-  (request: express.Request, response: express.Response) => {
-    return response.status(501).json({
-      data: "no tweet found",
-    });
-  }
-);
+router.get("/:tweetid", getTweetbyID);
+router.delete("/:id", deleteTweetByID);
+router.put("/:id");
 
 export default router;
